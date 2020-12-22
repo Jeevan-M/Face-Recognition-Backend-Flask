38e29796ada8d3563530da5928916a4730da0f31
Jeevan-M <csejeevanm@gmail.com>
2020-12-22 13:20:36 +0530

    env variable added using uwsgi

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ from addToDB import UserToDB
 from checkFace import CheckUserFace
 from flask_restful import Api
 from flask_cors import CORS
-import os
+
 
 app = Flask(__name__)
 CORS(app)
 endPointApi = Api(app)
-portNumber = int(os.environ.get('PORT', 5000))
 
 
 @app.route('/')
@@ -21,4 +20,4 @@ endPointApi.add_resource(UserToDB, '/saveUser/<string:check>')
 
 
 if __name__ == "__main__":
-    app.run(port=portNumber, debug=True)
+    app.run(debug=True)
