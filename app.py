cad69cc17bddd66c5166cb2d08aac5753e23316b
Jeevan-M <csejeevanm@gmail.com>
2020-12-22 13:09:33 +0530

    env variable added

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from addToDB import UserToDB
 from checkFace import CheckUserFace
 from flask_restful import Api
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
 endPointApi = Api(app)
+portNumber = int(os.environ.get('PORT', 5000))
 
 
 @app.route('/')
@@ -19,4 +21,4 @@ endPointApi.add_resource(UserToDB, '/saveUser/<string:check>')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=portNumber, debug=True)
