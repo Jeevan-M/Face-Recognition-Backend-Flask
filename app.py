1242538789ed8d4d55d45109529b4aab4d6d0c1a
Jeevan-M <csejeevanm@gmail.com>
2020-12-22 13:39:50 +0530

    error log is added

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from addToDB import UserToDB
 from checkFace import CheckUserFace
 from flask_restful import Api
 from flask_cors import CORS
-
+import logging
+import sys
 
 app = Flask(__name__)
 CORS(app)
 endPointApi = Api(app)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 
 @app.route('/')
